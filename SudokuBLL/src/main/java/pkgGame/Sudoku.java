146e0d27750b1c424041d0e72139ea052f76527b
qskau <qskau@10.0.0.128>
2018-10-03 10:37:12 -0400

    Initial Commit

---
+++ SudokuBLL/src/main/java/pkgGame/Sudoku.java
@@ -0,0 +1,580 @@
+package pkgGame;
+
+
+import java.security.SecureRandom;
+import java.util.*;
+import pkgEnum.ePuzzleViolation;
+import pkgHelper.LatinSquare;
+import pkgHelper.PuzzleViolation;
+
+/**
+ * Sudoku - This class extends LatinSquare, adding methods, constructor to
+ * handle Sudoku logic
+ * 
+ * @version 1.2
+ * @since Lab #2
+ * @author Bert.Gibbons
+ *
+ */
+public class Sudoku extends LatinSquare {
+
+	/**
+	 * 
+	 * iSize - the length of the width/height of the Sudoku puzzle.
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 */
+	private int iSize;
+
+	/**
+	 * iSqrtSize - SquareRoot of the iSize. If the iSize is 9, iSqrtSize will be
+	 * calculated as 3
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 */
+
+	private int iSqrtSize;
+
+	/**
+	 * Sudoku - for Lab #2... do the following:
+	 * 
+	 * set iSize If SquareRoot(iSize) is an integer, set iSqrtSize, otherwise throw
+	 * exception
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param iSize-
+	 *            length of the width/height of the puzzle
+	 * @throws Exception
+	 *             if the iSize given doesn't have a whole number square root
+	 */
+	public Sudoku(int iSize) throws Exception {
+
+		this.iSize = iSize;
+
+		double SQRT = Math.sqrt(iSize);
+		if ((SQRT == Math.floor(SQRT)) && !Double.isInfinite(SQRT)) {
+			this.iSqrtSize = (int) SQRT;
+		} else {
+			throw new Exception("Invalid size");
+		}
+
+		int[][] puzzle = new int[iSize][iSize];
+		super.setLatinSquare(puzzle);
+		
+		FillDiagonalRegions();
+	}
+
+	/**
+	 * Sudoku - pass in a given two-dimensional array puzzle, create an instance.
+	 * Set iSize and iSqrtSize
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param puzzle
+	 *            - given (working) Sudoku puzzle. Use for testing
+	 * @throws Exception
+	 *             will be thrown if the length of the puzzle do not have a whole
+	 *             number square root
+	 */
+	public Sudoku(int[][] puzzle) throws Exception {
+		super(puzzle);
+		this.iSize = puzzle.length;
+		double SQRT = Math.sqrt(iSize);
+		if ((SQRT == Math.floor(SQRT)) && !Double.isInfinite(SQRT)) {
+			this.iSqrtSize = (int) SQRT;
+		} else {
+			throw new Exception("Invalid size");
+		}
+
+	}
+
+	/**
+	 * getPuzzle - return the Sudoku puzzle
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @return - returns the LatinSquare instance
+	 */
+	public int[][] getPuzzle() {
+		return super.getLatinSquare();
+	}
+
+	/**
+	 * getRegionNbr - Return region number based on given column and row
+	 * 
+	 * 
+	 * Example, the following Puzzle:
+	 * 
+	 * 0 1 2 3 <br>
+	 * 1 2 3 4 <br>
+	 * 3 4 1 2 <br>
+	 * 4 1 3 2 <br>
+	 * 
+	 * getRegionNbr(3,0) should return a value of 1
+	 * 
+	 * @param iCol - Given column number
+	 * @param iRow - Given row number
+	 * @version 1.3
+	 * @since Lab #3
+	 * 
+	 * @return - return region number based on given column and row
+	 */
+	public int getRegionNbr(int iCol, int iRow) {
+
+		int i = (iCol / iSqrtSize) + ((iRow / iSqrtSize) * iSqrtSize);
+
+		return i;
+	}
+
+	/**
+	 * getRegion - figure out what region you're in based on iCol and iRow and call
+	 * getRegion(int)<br>
+	 * 
+	 * Example, the following Puzzle:
+	 * 
+	 * 0 1 2 3 <br>
+	 * 1 2 3 4 <br>
+	 * 3 4 1 2 <br>
+	 * 4 1 3 2 <br>
+	 * 
+	 * getRegion(0,3) would call getRegion(1) and return [2],[3],[3],[4]
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param iCol
+	 *            given column
+	 * @param iRow
+	 *            given row
+	 * @return - returns a one-dimensional array from a given region of the puzzle
+	 */
+	public int[] getRegion(int iCol, int iRow) {
+
+		int i = (iCol / iSqrtSize) + ((iRow / iSqrtSize) * iSqrtSize);
+
+		return getRegion(i);
+	}
+
+	/**
+	 * getRegion - pass in a given region, get back a one-dimensional array of the
+	 * region's content<br>
+	 * 
+	 * Example, the following Puzzle:
+	 * 
+	 * 0 1 2 3 <br>
+	 * 1 2 3 4 <br>
+	 * 3 4 1 2 <br>
+	 * 4 1 3 2 <br>
+	 * 
+	 * getRegion(2) and return [3],[4],[4],[1]
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param r
+	 *            given region
+	 * @return - returns a one-dimensional array from a given region of the puzzle
+	 */
+
+	public int[] getRegion(int r) {
+
+		int[] reg = new int[super.getLatinSquare().length];
+
+		int i = (r % iSqrtSize) * iSqrtSize;
+		int j = (r / iSqrtSize) * iSqrtSize;
+		int iMax = i + iSqrtSize;
+		int jMax = j + iSqrtSize;
+		int iCnt = 0;
+
+		for (; j < jMax; j++) {
+			for (i = (r % iSqrtSize) * iSqrtSize; i < iMax; i++) {
+				reg[iCnt++] = super.getLatinSquare()[j][i];
+			}
+		}
+
+		return reg;
+	}
+
+	/**
+	 * isPartialSudoku - return 'true' if...
+	 * 
+	 * It's a LatinSquare If each region doesn't have duplicates If each element in
+	 * the first row of the puzzle is in each region of the puzzle At least one of
+	 * the elemnts is a zero
+	 * 
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @return true if the given puzzle is a partial sudoku
+	 */
+	public boolean isPartialSudoku() {
+
+		super.setbIgnoreZero(true);
+		
+		super.ClearPuzzleViolation();
+		
+		if (hasDuplicates())
+			return false;
+
+		if (!ContainsZero()) {
+			super.AddPuzzleViolation(new PuzzleViolation(ePuzzleViolation.MissingZero, -1));
+			return false;
+		}
+		return true;
+
+	}
+
+	/**
+	 * isSudoku - return 'true' if...
+	 * 
+	 * Is a partialSudoku Each element doesn't a zero
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @return - returns 'true' if it's a partialSudoku, element match (row versus column) and no zeros
+	 */
+	public boolean isSudoku() {
+
+		this.setbIgnoreZero(false);
+		
+		super.ClearPuzzleViolation();
+		
+		if (hasDuplicates())
+			return false;
+		
+		if (!super.isLatinSquare())
+			return false;
+		
+		for (int i = 1; i < super.getLatinSquare().length; i++) {
+
+			if (!hasAllValues(getRow(0), getRegion(i))) {
+				return false;
+			}
+		}
+
+		if (ContainsZero()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * isValidValue - test to see if a given value would 'work' for a given column /
+	 * row
+	 * 
+	 * @version 1.2
+	 * @since Lab #2
+	 * @param iCol
+	 *            puzzle column
+	 * @param iRow
+	 *            puzzle row
+	 * @param iValue
+	 *            given value
+	 * @return - returns 'true' if the proposed value is valid for the row and column
+	 */
+	public boolean isValidValue(int iCol, int iRow, int iValue) {
+		
+		if (doesElementExist(super.getRow(iRow),iValue))
+		{
+			return false;
+		}
+		if (doesElementExist(super.getColumn(iCol),iValue))
+		{
+			return false;
+		}
+		if (doesElementExist(this.getRegion(iCol, iRow),iValue))
+		{
+			return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * PrintPuzzle This method will print the puzzle to the console (space between
+	 * columns, line break after row)
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 */
+	public void PrintPuzzle() {
+		for (int i = 0; i < this.getPuzzle().length; i++) {
+			System.out.println("");
+			for (int j = 0; j < this.getPuzzle().length; j++) {
+				System.out.print(this.getPuzzle()[i][j]);
+				if ((j + 1) % iSqrtSize == 0)
+					System.out.print(" ");
+			}
+			if ((i + 1) % iSqrtSize == 0)
+				System.out.println(" ");
+
+		}
+		System.out.println("");
+	}
+
+	/**
+	 * FillDiagonalRegions - After the puzzle is created, set the diagonal regions
+	 * with random values
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 */
+	private void FillDiagonalRegions() {
+
+		for (int i = 0; i < iSize; i = i + iSqrtSize) {
+			System.out.println("Filling region: " + getRegionNbr(i, i));
+			SetRegion(getRegionNbr(i, i));
+			ShuffleRegion(getRegionNbr(i, i));
+		}
+	}
+
+	/**
+	 * SetRegion - purpose of this method is to set the values of a given region
+	 * (they will be shuffled later)
+	 * 
+	 * Example, the following Puzzle start state:
+	 * 
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 
+	 * SetRegion(2) would transform the Puzzle to:<br>
+	 * 
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 1 2 0 0 <br>
+	 * 3 4 0 0 <br>
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 * @param r
+	 *            - Given region number
+	 */
+	private void SetRegion(int r) {
+		int iValue = 0;
+
+		iValue = 1;
+		for (int i = (r / iSqrtSize) * iSqrtSize; i < ((r / iSqrtSize) * iSqrtSize) + iSqrtSize; i++) {
+			for (int j = (r % iSqrtSize) * iSqrtSize; j < ((r % iSqrtSize) * iSqrtSize) + iSqrtSize; j++) {
+				this.getPuzzle()[i][j] = iValue++;
+			}
+		}
+	}
+
+	/**
+	 * SetRegion - purpose of this method is to set the values of a given region
+	 * (they will be shuffled later)
+	 * 
+	 * Example, the following Puzzle start state:
+	 * 
+	 * 1 2 0 0 <br>
+	 * 3 4 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 
+	 * ShuffleRegion(0) might transform the Puzzle to:<br>
+	 * 
+	 * 2 3 0 0 <br>
+	 * 1 4 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 0 0 0 0 <br>
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 * @param r
+	 *            - Given region number
+	 */
+	private void ShuffleRegion(int r) {
+		int[] region = getRegion(r);
+		shuffleArray(region);
+		int iCnt = 0;
+		for (int i = (r / iSqrtSize) * iSqrtSize; i < ((r / iSqrtSize) * iSqrtSize) + iSqrtSize; i++) {
+			for (int j = (r % iSqrtSize) * iSqrtSize; j < ((r % iSqrtSize) * iSqrtSize) + iSqrtSize; j++) {
+				this.getPuzzle()[i][j] = region[iCnt++];
+			}
+		}
+	}
+
+	/**
+	 * shuffleArray this method will shuffle a given one-dimension array
+	 * 
+	 * @version 1.3
+	 * @since Lab #3
+	 * @param ar
+	 *            given one-dimension array
+	 */
+	private void shuffleArray(int[] ar) {
+
+		Random rand = new SecureRandom();
+		for (int i = ar.length - 1; i > 0; i--) {
+			int index = rand.nextInt(i + 1);
+			// Simple swap
+			int a = ar[index];
+			ar[index] = ar[i];
+			ar[i] = a;
+		}
+	}
+
+		
+	
+	public void FillRemaining() 
+	{
+		int[][] myTempSudoku1;
+		int iterations = 1;
+			//Tracking the number of iterations until have working Sudoku puzzle
+		Creation();
+		
+		while(isSudoku() == false)
+		{
+			iterations++;
+			myTempSudoku1 = super.getLatinSquare();
+			
+			
+			for (int i=0;i<iSize;i++)
+			{
+				for (int j=0;j<iSize;j++)
+				{
+					myTempSudoku1[i][j] = 0;
+					//Need to clear the values in each index and start over
+					//Bypass the try catch method for making a new Sudoku
+				}
+			}
+			super.setLatinSquare(myTempSudoku1);
+			FillDiagonalRegions();
+			Creation();
+			
+		}
+		System.out.println("Number of iterations until working Sudoku: "+iterations);
+	}
+	
+	private void Creation() {
+		
+		int iSquareSize = iSize*iSize;
+			//Square of one side
+		
+		int countPoss;
+			//Count of possible values for a given index
+		
+		int matchMinCount = 0;
+			//Number of indices that share the minimum count of possible values
+		
+		int minPossVals = iSize;//initialization
+			//Used to track the count of minimum possible values
+		
+		int[][] myTempSudoku;
+			//Used to set the Sudoku after each iteration
+		
+		int maxA = iSquareSize - iSqrtSize*iSize;
+			//Max number of empty indices after filling the diagonal regions
+		
+		
+		
+		for (int a = 0;a<maxA;a++)
+			//For loop that iterates over the count of empty indeices
+		{
+		int[] rowSave = new int[iSquareSize];
+			//Tracks iRow
+		
+		int[] colSave = new int[iSquareSize];
+			//Track iCol
+		
+		int[][][] trackPoss = new int[iSize][iSize][iSize];
+			//[row][column][possible value]
+			//3-D array of the possible values for each index
+			//Have to recreate for every iteration
+		
+		for (int iRow=0;iRow<iSize;iRow++)
+			
+		{
+			for (int iCol=0;iCol<iSize;iCol++)
+				
+			{
+				if (super.getLatinSquare()[iRow][iCol] == 0 )
+					
+				{
+					countPoss = 0;
+						//Resetting the count of possibilities for a given index
+					
+					for (int k=1;k<=iSize;k++)
+					{
+						
+						if (isValidValue(iCol,iRow,k) == true)
+						{
+							trackPoss[iRow][iCol][countPoss] = k;
+							countPoss++;
+						}
+						
+					}
+					
+					if (countPoss == 0)
+						break;
+						//Only works if add this statement in
+						//DON'T KNOW WHY
+					
+					if (countPoss == minPossVals)
+					{
+						matchMinCount++;
+						rowSave[matchMinCount] = iRow;
+						colSave[matchMinCount] = iCol;
+					}
+					
+					if(countPoss < minPossVals)
+					{
+						minPossVals = countPoss;
+							//Need to reset minPossVals
+							//Will always do this first because already setting the diagonals
+							//Therefore, the maximum number of possibilities will always be less than iSize
+						
+						matchMinCount = 0;
+						colSave = new int[iSquareSize];
+						rowSave = new int[iSquareSize];
+						rowSave[matchMinCount] = iRow;
+						colSave[matchMinCount] = iCol;
+					}
+				}
+			}
+		}
+		
+		
+		
+		
+		
+		System.out.println("");
+		System.out.println("New a: "+a);
+		System.out.println("minPossVals: "+minPossVals);
+			//Once the minPossVals becomes 0, the a random Sudoku cannot be created
+		
+		
+		Random rand = new SecureRandom();
+		int indexOfMins = rand.nextInt((matchMinCount+1));
+			//The index corresponding to the possible indices that have the minimum amount of possible values
+		
+		
+		rand = new SecureRandom();
+		int indexOfPoss = rand.nextInt((minPossVals));
+			//The index corresponding to a possible value for the selected row and column
+		 
+		
+		int rowToSet = rowSave[indexOfMins];
+		System.out.println("rowToSet: "+rowToSet);
+		
+		int colToSet = colSave[indexOfMins];
+		System.out.println("colToSet: "+colToSet);
+		
+		int valueToSet = trackPoss[rowToSet][colToSet][indexOfPoss];		
+		System.out.println("valueToSet: "+ valueToSet);
+		
+		myTempSudoku = super.getLatinSquare();
+		myTempSudoku[rowToSet][colToSet] = valueToSet;
+		super.setLatinSquare(myTempSudoku);
+		minPossVals = iSize;
+		
+		PrintPuzzle();
+		}
+		
+	}
+
+}
